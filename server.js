8614d365bb157d6813db72c3763b1ed24f967253
Clement Lork <clement_lork@mymail.sutd.edu.sg>
2018-09-17 21:21:59 +0800

    added_qr_scanning_library

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(session({ secret: 'hellowhoisthis' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+//app.use(passport.initialize());
+//app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 ///middleware for images/js scripts
