93632e71987c4c2e25cd29342f74fe0deff13a43
Clement Lork <clement_lork@mymail.sutd.edu.sg>
2018-10-01 21:56:13 +0800

    addedcheckbox

--- models/routes.js
+++ models/routes.js
@@ -14,27 +14,28 @@ module.exports = function(app,sfconn) {
     app.post('/index', async (req, res) => {
         console.log(req.body.customers.split(',#,'));
         console.log('dfsdf');
-        var data_array = new Array(req.body.customers.split(',#,').length).fill(null).map(()=>new Array(21).fill(null));
+        var data_array = new Array(req.body.customers.split(',#,').length).fill(null).map(()=>new Array(23).fill(null));
         
-        ///send to salesforce
-        // for (i=0;i<req.body.customers.split(',#,').length;i++){
-        //     data_array[i]=req.body.customers.split(',#,')[i].split(',')
-        //     sfconn.sobject("Lead").create({
-        //         email : data_array[i][14],
-        //         firstname : data_array[i][9],
-        //         lastname : data_array[i][10],
-        //         title : data_array[i][11],
-        //         company : data_array[i][12],
-        //         leadsource: 'P2C Helper',
-        //         phone: '+65'+data_array[i][16],
-        //         ProductInterest__c: data_array[i][0],
-        //         Description: 'Product Manager: '+ data_array[i][6] 
-        //     }, function(err, ret) {
-        //     if (err || !ret.success) { 
-        //         return console.error(err, ret); 
-        //     }
-        //     });
-        // };   
+        
+        for (i=0;i<req.body.customers.split(',#,').length;i++){
+            data_array[i]=req.body.customers.split(',#,')[i].split(',')
+            //send to salesforce 
+            // sfconn.sobject("Lead").create({
+            //     email : data_array[i][14],
+            //     firstname : data_array[i][9],
+            //     lastname : data_array[i][10],
+            //     title : data_array[i][11],
+            //     company : data_array[i][12],
+            //     leadsource: 'P2C Helper',
+            //     phone: '+65'+data_array[i][16],
+            //     ProductInterest__c: data_array[i][0],
+            //     Description: 'Product Manager: '+ data_array[i][6] 
+            // }, function(err, ret) {
+            // if (err || !ret.success) { 
+            //     return console.error(err, ret); 
+            // }
+            // });
+        };   
 
         ///send to gsheets
         let  arry =  data_array[req.body.customers.split(',#,').length-1];
